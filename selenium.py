7c4d56bde57049f70eec90dc2e2d49fe3f5e5272
Nivedhan Senthilkumar <70594428+NivedhanSenthilkumar@users.noreply.github.com>
2022-05-07 15:03:49 +0530

    git commit selenium.py

--- selenium.py
+++ selenium.py
@@ -3,4 +3,21 @@
 """LIBRARIES"""
 import pandas as pd
 import numpy as np
-import selenium.webdriver as  sd
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+import pandas
+import time
+
+
+#1-AMAZON
+
+
+
+
+
+
+
+
